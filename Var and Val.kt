19b47f4439400c1c9fdc96a906ab9eb50593b548
darya <64057603+darya1380@users.noreply.github.com>
2020-08-04 09:14:05 +0430

    Add files via upload

---
+++ Var and Val.kt
@@ -0,0 +1,15 @@
+
+fun main()
+{
+    var a = 12;
+    val b = 13;
+
+//    var value can be changed
+    a = 14;
+//    but the type is fixed
+//    a = "hello world" Compile error
+
+//    val value can't be changed once assigned
+//    b = 14   Compile error
+    
+}
